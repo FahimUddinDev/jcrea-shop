2473f51e7d445e20947cfb24594a7f272bb8ec9c
fcode9 <faysalmahamud186@gmail.com>
2026-08-18 22:31:45 +0600

    update secret

--- auth.ts
+++ auth.ts
@@ -5,7 +5,9 @@ import { cookies } from "next/headers";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   trustHost: true,
-  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
+  secret:
+    process.env.AUTH_SECRET ||
+    process.env.NEXTAUTH_SECRET
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
